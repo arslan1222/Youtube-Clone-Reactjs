b129b4fd718c000b4775fa45572ed600f46078c1
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-01-12 22:05:59 +0500

    Feed section added

---
+++ youtube-clone/src/Components/Feed/Feed.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import './Feed.css'
+import { assets } from '../../assets/assets'
+
+const Feed = () => {
+  return (
+    <div className="feed">
+        <div className='card'>
+            <img src={assets.thumbnail1} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail2} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail3} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail4} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail5} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail6} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail7} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail8} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail1} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail2} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail3} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail4} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail5} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail6} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail7} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+        <div className='card'>
+            <img src={assets.thumbnail8} alt="" />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
+            <h3>How i Grow</h3>
+            <p>36K views &bull; 5 days ago</p>
+        </div>
+    </div>
+  )
+}
+
+export default Feed
