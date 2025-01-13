2192058e68771f22c888f6cd1f3c9634874a73fa
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-01-13 13:17:25 +0500

    Home data integrated with adding catgories using Youtube API

--- youtube-clone/src/Components/Feed/Feed.jsx
+++ youtube-clone/src/Components/Feed/Feed.jsx
@@ -1,107 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { valueConverter } from '../../data'
+import moment from 'moment'
 
-const Feed = () => {
+const Feed = ({category}) => {
+
+    const [data, setData] = useState([]);
+
+    const API_KEY = import.meta.env.VITE_API_BASE_URL;
+    const fetchData = async () => {
+        const videoListUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+        await fetch(videoListUrl).then(response=>response.json()).then(data=>setData(data.items));
+    }
+
+    useEffect(()=>{
+        fetchData();
+    }, [category])
+
   return (
     <div className="feed">
-        <Link to={`video/20/34232`} className='card'>
-            <img src={assets.thumbnail1} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </Link>
-        <div className='card'>
-            <img src={assets.thumbnail2} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail3} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail4} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail5} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail6} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail7} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail8} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail1} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail2} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail3} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail4} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail5} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail6} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail7} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
-        <div className='card'>
-            <img src={assets.thumbnail8} alt="" />
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ratione.</h2>
-            <h3>How i Grow</h3>
-            <p>36K views &bull; 5 days ago</p>
-        </div>
+        {data.map((item, index) => {
+        return (
+          <Link to={`video/${item.snippet.categoryId}/${item.id}`} className="card">
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>{valueConverter(item.statistics.viewCount)} views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
+          </Link>
+        );
+      })}
     </div>
   )
 }
--- youtube-clone/src/Components/Sidebar/Sidebar.jsx
+++ youtube-clone/src/Components/Sidebar/Sidebar.jsx
@@ -2,32 +2,35 @@ import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets.js'
 
-const Sidebar = ({sidebar}) => {
+const Sidebar = ({sidebar, category, setCategory}) => {
   return (
     <div className={`sidebar ${sidebar ? '' : 'small-sidebar'}`}>
       <div className="shortcut-links">
-        <div className="side-link">
+        <div onClick={()=>setCategory(0)} className={`side-link ${category === 0 ? 'active' : ''}`}>
           <img src={assets.home} alt="" /> <p>Home</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(20)} className={`side-link ${category === 20 ? 'active' : ''}`}>
           <img src={assets.game_icon} alt="" /> <p>Gaming</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(2)} className={`side-link ${category === 2 ? 'active' : ''}`}>
           <img src={assets.automobiles} alt="" /> <p>Automobiles</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(17)} className={`side-link ${category === 17 ? 'active' : ''}`}>
           <img src={assets.sports} alt="" /> <p>Sports</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(24)} className={`side-link ${category === 24 ? 'active' : ''}`}>
           <img src={assets.entertainment} alt="" /> <p>Entertainement</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(28)} className={`side-link ${category === 28 ? 'active' : ''}`}>
           <img src={assets.tech} alt="" /> <p>Technology</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(10)} className={`side-link ${category === 10 ? 'active' : ''}`}>
+          <img src={assets.music} alt="" /> <p>Music</p>
+        </div>
+        <div onClick={()=>setCategory(22)} className={`side-link ${category === 22 ? 'active' : ''}`}>
           <img src={assets.blogs} alt="" /> <p>Blogs</p>
         </div>
-        <div className="side-link">
+        <div onClick={()=>setCategory(25)} className={`side-link ${category === 25 ? 'active' : ''}`}>
           <img src={assets.news} alt="" /> <p>News</p>
         </div>
         <hr />
