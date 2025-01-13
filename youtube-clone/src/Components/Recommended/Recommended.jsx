a84c20ad77d2ff947c6e53ad3074babfe3c89a3f
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-01-13 10:18:15 +0500

    Recommended section added, and frontend compeleted.. Youtube API integration started

---
+++ youtube-clone/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import './Recommended.css'
+import { assets } from '../../assets/assets.js'
+
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+        <div className="side-video-list">
+            <img src={assets.thumbnail1} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail2} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail3} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail4} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail5} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail6} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail7} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={assets.thumbnail8} alt="" />
+            <div className="vid-info">
+                <h4>Best channel to know, How to grow</h4>
+                <p>How i Grow</p>
+                <p>153K views &bull; 2 days ago</p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Recommended
